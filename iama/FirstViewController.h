d8320b79d7dbfa5f73a1d2f706eb0011cfa1206c
Vicent <huanggang1109@163.com>
2015-08-28 17:25:57 +0800

    Initial Commit

---
+++ iama/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  iama
+//
+//  Created by Vicent on 15/8/28.
+//  Copyright (c) 2015年 Vicent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ iama/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  iama
+//
+//  Created by Vicent on 15/8/28.
+//  Copyright (c) 2015年 Vicent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
